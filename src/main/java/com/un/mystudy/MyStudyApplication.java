ecd2ba84d4345acafdbd7976498f6c92714995e2
un <66024439+mapzine123@users.noreply.github.com>
2023-08-02 18:32:40 +0900

    inital commit

---
+++ src/main/java/com/un/mystudy/MyStudyApplication.java
@@ -0,0 +1,13 @@
+package com.un.mystudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyStudyApplication.class, args);
+    }
+
+}
